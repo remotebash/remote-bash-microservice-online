3520f37ce62888b7704e0bfbd020028d9cfa87e6
Aluno <lucasizj@gmail.com>
2019-10-21 20:53:08 -0200

    Teste Heroku

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const banco = require('./banco/Banco')
-const send = require('./Send')
+// const banco = require('./banco/Banco')
+// const send = require('./Send')
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -9,15 +9,16 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-app.post('/maquina', function (req, res) {
-  banco.salvarStatus(req.body)
-  res.end();
-})
+// app.post('/maquina', function (req, res) {
+//   banco.salvarStatus(req.body)
+//   res.end();
+// })
 app.get('/status/:id', function (req, res) {
-  banco.pegarStatus(req.params).then(x=>{
-    res.end(x);
-  })
+  // banco.pegarStatus(req.params).then(x=>{
+  //   res.end(x);
+  // })
+  res.end("id: " + req.params.id);
 })
 
-app.listen(8080)
-console.log("Main server runing on Port:" + 8080);
+app.listen(process.env.PORT)
+console.log("Main server runing on Port:" + process.env.PORT);
