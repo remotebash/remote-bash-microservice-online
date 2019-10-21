5b02d72480ce2646875b7a1e522b9da9f65d0074
Aluno <lucasizj@gmail.com>
2019-10-21 21:51:15 -0200

    vouChorar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const banco = require('./banco/Banco')
+// const banco = require('./banco/Banco')
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -8,18 +8,18 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-app.post('/maquina', function (req, res) {
-  banco.salvarStatus(req.body)
-  res.end();
-})
+// app.post('/maquina', function (req, res) {
+//   banco.salvarStatus(req.body)
+//   res.end();
+// })
 app.get('', (req,res)=>{
   res.end("Aplication Up!");
 })
-app.get('/status/:id', function (req, res) {
-  banco.pegarStatus(req.params).then(x=>{
-    res.end(x);
-  })
-})
+// app.get('/status/:id', function (req, res) {
+//   banco.pegarStatus(req.params).then(x=>{
+//     res.end(x);
+//   })
+// })
 
 app.listen(process.env.PORT)
 console.log("Main server runing on Port:" + 8080);
