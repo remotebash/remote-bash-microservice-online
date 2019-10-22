bd8dd652f8a5ce540143165409a1b4137abae599
Aluno <lucasizj@gmail.com>
2019-10-22 20:37:31 -0200

    vai desgraça fdp 3

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-//const banco = require('./banco/Banco')
-//var Mongoose = require('Mongoose');
+const banco = require('./banco/Banco')
+
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -9,18 +9,18 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-// app.post('/maquina', function (req, res) {
-//   banco.salvarStatus(req.body)
-//   res.end();
-// })
+app.post('/maquina', function (req, res) {
+  banco.salvarStatus(req.body)
+  res.end();
+})
 app.get('', (req,res)=>{
   res.end("Aplication Up!");
 })
-// app.get('/status/:id', function (req, res) {
-//   banco.pegarStatus(req.params).then(x=>{
-//     res.end(x);
-//   })
-// })
+app.get('/status/:id', function (req, res) {
+  banco.pegarStatus(req.params).then(x=>{
+    res.end(x);
+  })
+})
 
 app.listen(process.env.PORT)
 console.log("Main server runing on Port:" + 8080);
--- banco/Banco.js
+++ banco/Banco.js
@@ -1,71 +1,71 @@
-//var Mongoose = require('Mongoose');
+var Mongoose = require('Mongoose');
 
-// var db = Mongoose.connection;
+var db = Mongoose.connection;
 
-// db.on('error', console.error);
-// db.once('open', function () {
-//     console.log('Conectado ao MongoDB.')
-//     // Vamos adicionar nossos Esquemas, Modelos e consultas aqui
+db.on('error', console.error);
+db.once('open', function () {
+    console.log('Conectado ao MongoDB.')
+    // Vamos adicionar nossos Esquemas, Modelos e consultas aqui
 
-// });
-// var statusSchema = new Mongoose.Schema({
-//     id: String,
-//     data: Number
-// });
-// var status = Mongoose.model('status', statusSchema);
+});
+var statusSchema = new Mongoose.Schema({
+    id: String,
+    data: Number
+});
+var status = Mongoose.model('status', statusSchema);
 
-// module.exports = {
-//     salvarStatus : (maquina => {
+module.exports = {
+    salvarStatus : (maquina => {
         
-//         let data = new Date();
-//         let statuParaSalvar = new status({
-//             'id' : maquina.id,
-//             'data' : data.getTime()
-//         });
-//         statuParaSalvar.save(function (err, usuarioParaSalvar) {
-//             if (err) return console.error(err);
-//         });
-//         console.log("salvou")
-//     }),
-//     pegarStatus: (maquina => {
-//         return new Promise(resolve=>{
-//             var query = status.findOne({ 'id': maquina.id }).sort('-data').select('data');
+        let data = new Date();
+        let statuParaSalvar = new status({
+            'id' : maquina.id,
+            'data' : data.getTime()
+        });
+        statuParaSalvar.save(function (err, usuarioParaSalvar) {
+            if (err) return console.error(err);
+        });
+        console.log("salvou")
+    }),
+    pegarStatus: (maquina => {
+        return new Promise(resolve=>{
+            var query = status.findOne({ 'id': maquina.id }).sort('-data').select('data');
             
-//             // selecting the `name` and `occupation` fields
+            // selecting the `name` and `occupation` fields
             
-//             let data = new Date();
+            let data = new Date();
             
-//             // execute the query at a later time
-//             query.exec(function (err, retorno) {
-//                 if (err) return handleError(err);
-//                 // Prints "Space Ghost is a talk show host."
-//                 if(retorno){
-//                     if(data.getTime() - retorno.data < 12000){
-//                         resolve(JSON.stringify({
-//                             'id' : maquina.id,
-//                             'status' : "online"
-//                         }))
-//                     } else {
-//                         resolve(JSON.stringify({
-//                             'id' : maquina.id,
-//                             'status' : "offline"
-//                         }))
-//                     }
-//                 } else{
-//                     resolve(JSON.stringify({
-//                         'id' : maquina.id,
-//                         'status' : "Not found"
-//                     }))
-//                 }
+            // execute the query at a later time
+            query.exec(function (err, retorno) {
+                if (err) return handleError(err);
+                // Prints "Space Ghost is a talk show host."
+                if(retorno){
+                    if(data.getTime() - retorno.data < 12000){
+                        resolve(JSON.stringify({
+                            'id' : maquina.id,
+                            'status' : "online"
+                        }))
+                    } else {
+                        resolve(JSON.stringify({
+                            'id' : maquina.id,
+                            'status' : "offline"
+                        }))
+                    }
+                } else{
+                    resolve(JSON.stringify({
+                        'id' : maquina.id,
+                        'status' : "Not found"
+                    }))
+                }
                 
-//             });
-//         })
-//     })
-// }
-// //Ta no grupo
-// var pass = "KlovisBash";
-// pass = encodeURIComponent(pass)
+            });
+        })
+    })
+}
+//Ta no grupo
+var pass = "KlovisBash";
+pass = encodeURIComponent(pass)
 
-// Mongoose.connect('mongodb+srv://RemoteSkeletom:' + pass + '@remotebash-9meuu.mongodb.net/test?retryWrites=true&w=majority');
+Mongoose.connect('mongodb+srv://RemoteSkeletom:' + pass + '@remotebash-9meuu.mongodb.net/test?retryWrites=true&w=majority');
 
 
